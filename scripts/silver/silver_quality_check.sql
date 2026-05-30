57ac0b88af1d0cff604c8cd6b5e7d4aa4fca13b3
erwin8989 <50455961+erwin8989@users.noreply.github.com>
2026-05-30 23:08:13 +0300

    Update silver_quality_check.sql

--- scripts/silver/silver_quality_check.sql
+++ scripts/silver/silver_quality_check.sql
@@ -84,3 +84,28 @@ SELECT
 *
 FROM silver.crm_sales_details
 WHERE sls_order_dt > sls_ship_dt OR sls_order_dt > sls_due_dt
+
+
+		--   ERP tables ---
+
+-- Identify Out-of-Range Dates
+SELECT DISTINCT
+bdate
+FROM bronze.erp_cust_az12
+WHERE bdate < '1924-01-01' OR bdate > GETDATE()
+
+SELECT DISTINCT
+bdate
+FROM silver.erp_cust_az12
+WHERE bdate < '1924-01-01' OR bdate > GETDATE()
+
+-- Data Standardization & Consistency
+SELECT DISTINCT 
+gen,
+CASE WHEN UPPER(TRIM(gen)) IN ('F', 'FEMALE') THEN 'Female'
+	 WHEN UPPER(TRIM(gen)) IN ('M', 'MALE') THEN 'Male'
+ELSE 'n/a'
+END AS gen
+FROM bronze.erp_cust_az12
+
+SELECT * FROM silver.erp_cust_az12
